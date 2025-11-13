1220498f745e1a9daddfd206530101fb8a524f0c
physiquate <physiquate@gmail.com>
2025-11-13 18:32:01 +0530

    Add files via upload

---
+++ CA/nov/CA_21-10-25.js
@@ -0,0 +1,226 @@
+
+  // =========================================== Step 1: Questions Array (inline LaTeX fixed) =====================================================
+window.questionSet = [
+{ 
+  q: "Which Indian state has declared 2025 as the 'Year of Urban Development'?", 
+  options: [
+    "Maharashtra",
+    "Uttar Pradesh",
+    "Gujarat",
+    "Madhya Pradesh"
+  ], 
+  answer: 2 
+},
+{ 
+  q: "What is the mascot of the 4th South Asian Athletics Championships?", 
+  options: [
+    "Sheru ",
+    "Dalma",
+    "Bheem",
+    "Tara"
+  ], 
+  answer: 1 
+},
+{ 
+  q: "The 11th Indian International Science Festival will be held in which city?", 
+  options: [
+    "Kolkata",
+    "Chandigarh",
+    "Chennai",
+    "Bengaluru"
+  ], 
+  answer: 1 
+},
+{ 
+  q: "On which date is World Statistics Day celebrated?", 
+  options: [
+    "18 October",
+    "20 October",
+    "22 October",
+    "25 October"
+  ],
+  answer: 1 
+},
+{ 
+  q: "The ‘Kati Bihu’ festival is mainly celebrated in which Indian state?", 
+  options: [
+    "Assam",
+    "Manipur",
+    "Tripura",
+    "West Bengal"
+  ],  
+  answer: 0 
+},
+{ 
+  q: "Which state ranked first in the ‘Mining Rich’ category of the State Mining Readiness Index 2025?", 
+  options: [
+    "Odisha",
+    "Jharkhand",
+    "Madhya Pradesh",
+    "Chhattisgarh"
+  ],
+  answer: 2 
+},
+{ 
+  q: "The 'We Rise' initiative has been launched by which organization?", 
+  options: [
+    "Ministry of Skill Development",
+    "Ministry of Education",
+    "NITI Aayog",
+    "Ministry of Labour and Employment"
+  ],
+  answer: 2 
+},
+{ 
+  q: "The Indian Coast Guard conducted the regional Search and Rescue Exercise (ResAREX-25) in which state?", 
+  options: [
+    "Tamil Nadu",
+    "Karnataka",
+    "Kerala",
+    "Goa"
+  ],
+  answer: 2 
+},
+{ 
+  q: "Which country became the first in Latin America to legalize euthanasia?", 
+  options: [
+    "Argentina",
+    "Uruguay",
+    "Brazil",
+    "Chile"
+  ], 
+  answer: 1 
+},
+{ 
+  q: "Who is the first Indian fast bowler to play 50 matches in all formats of international cricket?", 
+  options: [
+    "Mohammed Shami",
+    "Jasprit Bumrah",
+    "Ishant Sharma",
+    "Bhuvneshwar Kumar"
+  ],
+  answer: 1 
+},
+{ 
+  q: "Who was awarded the D. V. Narasimha Rao Award recently?", 
+  options: [
+    "P. Chidambaram",
+    "Manmohan Singh",
+    "Raghuram Rajan",
+    "Nirmala Sitharaman"
+  ],
+  answer: 1 
+},
+{ 
+  q: "Which medal did India win in the 13th edition of the Sultan of Johor Cup 2025 held in Malaysia?", 
+  options: [
+    "Gold",
+    "Silver",
+    "Bronze",
+    "None"
+  ],
+  answer: 1 
+},
+{ 
+  q: "On which date is Police Commemoration Day (Police Memorial Day) observed in India?", 
+  options: [
+    "18 October",
+    "20 October",
+    "21 October",
+    "25 October"
+  ],
+  answer: 2 
+},
+{ 
+  q: "Which country won the 24th edition of the FIFA U-20 World Cup title?", 
+  options: [
+    "Argentina",
+    "Italy",
+    "Morocco",
+    "Spain"
+  ],
+  answer: 2 
+},
+{ 
+  q: "Who became the first Indian Managing Director and CEO of Hyundai Motor India?", 
+  options: [
+    "Tarun Garg",
+    "Shailesh Chandra",
+    "Sanjay Gupta",
+    "Rajesh Goel"
+  ],
+  answer: 0 
+},
+{ 
+  q: "Who won the 125th edition of the IFA Shield 2025 title?", 
+  options: [
+    "East Bengal FC",
+    "Mohun Bagan Super Giant",
+    "Mohammedan SC",
+    "Bengaluru FC"
+  ],
+  answer: 1 
+},
+{ 
+  q: "Which organization released the first-ever State of Finance for Forests 2025 (Reboot) report?", 
+  options: [
+    "World Bank",
+    "UNEP",
+    "WWF",
+    "FAO"
+  ],
+  answer: 1 
+},
+{ 
+  q: "The All India Institute of Ayurveda celebrated which Foundation Day on 17 October 2025?", 
+  options: [
+    "5th",
+    "7th",
+    "9th",
+    "10th"
+  ],
+  answer: 2 
+},
+{ 
+  q: "Who won the US Grand Prix 2025 title?", 
+  options: [
+    "Lewis Hamilton",
+    "Lando Norris",
+    "Max Verstappen",
+    "Charles Leclerc"
+  ],
+  answer: 2 
+},
+{ 
+  q: "Where will the 2nd World Summit on Social Development be held?", 
+  options: [
+    "Geneva",
+    "Doha",
+    "New Delhi",
+    "Paris"
+  ], 
+  answer: 1 
+},
+{ 
+  q: "Which Indian state launched a large-scale Bamboo Plantation Project in 2025?", 
+  options: [
+    "Telangana",
+    "Assam",
+    "Odisha",
+    "Kerala"
+  ], 
+  answer: 2
+},
+{ 
+  q: "Who conducted the world’s first live underwater interview?", 
+  options: [
+    "Emmanuel Macron",
+    "Surangel Whipps Jr",
+    "Volodymyr Zelensky",
+    "António Guterres"
+  ],
+  answer: 1
+}
+  
+    
+];
