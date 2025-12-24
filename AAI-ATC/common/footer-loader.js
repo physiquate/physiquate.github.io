f4853c8f3e555651d37b830a03906ef0ca656ac0
physiquate <physiquate@gmail.com>
2025-12-24 23:05:38 +0530

    Delete AAI-ATC/common directory

--- AAI-ATC/common/footer-loader.js
+++
@@ -1,8 +0,0 @@
-fetch("../common/footer.html")
-  .then(res=>res.text())
-  .then(html=>{
-    document.getElementById("footer").innerHTML = html;
-    const yearEl = document.getElementById("year");
-    if(yearEl) yearEl.textContent = new Date().getFullYear();
-  })
-  .catch(err=>console.error("Footer load failed:", err));
